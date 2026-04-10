fc2ecb409963af7ccc07445123f246d0c739ee85
Green Target Academy <amine@onementorgroup.com>
2026-04-10 03:26:07 +0200

    latest port

--- Back/server.js
+++ Back/server.js
@@ -158,7 +158,7 @@ import subscriptionRoutes from './routes/subscriptionRoutes.js'; // Subscription
 dotenv.config();
 
 const app = express();
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 10000;
 const mongoUri = process.env.MONGO_URI;
 
 const allowedOrigins = [
